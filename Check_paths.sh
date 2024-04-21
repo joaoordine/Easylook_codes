591253d2c6de03a768412adc98ec8d053db8bee5
João Vitor Wagner Ordine <67348033+joaoordine@users.noreply.github.com>
2024-04-21 14:31:24 -0300

    Create Check_paths.sh

---
+++ Check_paths.sh
@@ -0,0 +1,7 @@
+### Define the directories containing genomes and hmm profiles 
+hmm_dir="/temporario2/11217468/projects/saureus_global/metabolism_analyses/fast_transporters"
+genome_dir="/temporario2/11217468/projects/saureus_global/QC_filtered_genome_files"
+
+### Debug: Print directory paths
+echo "hmm_dir: $hmm_dir"
+echo "genome_dir: $genome_dir"
